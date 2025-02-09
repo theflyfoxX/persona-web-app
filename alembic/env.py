f1a82ec4cf2111978a60c08de0024237010d5d70
theflyfoxX <rassasali01@gmail.com>
2025-02-09 14:32:30 +0100

    Change from DB test to mocking tests

--- alembic/env.py
+++ alembic/env.py
@@ -2,9 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from app.database import Base
+from app.config.database_postgres import Base
 from alembic import context
-from app.configuration import settings
+from app.config.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
